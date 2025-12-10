d54499a5674d92f385adc41890131df8fd6005c2
boeani05 <flyingdoge@gmx.at>
2025-12-10 15:32:45 +0100

    Implement Doubly Linked List for Playlist operations
    Develops a custom Doubly Linked List (DLL) to manage music songs.
    Includes robust implementations for:
    - Adding songs (at end, at specific position, at beginning).
    - Deleting songs (by title, by index, first, last).
    - Navigating songs (next, previous).
    - Playing the current song.
    - Checking playlist status (empty, song exists).
    Handles all edge cases (empty list, single element, head/tail operations)
    and maintains current song state.

--- src/Playlist.java
+++ src/Playlist.java
@@ -56,6 +56,7 @@ public class Playlist {
                 head.previous = newNode;
                 newNode.next = head;
                 head = newNode;
+                currentSongNode = head;
             }
 
             size++;
@@ -88,5 +89,244 @@ public class Playlist {
 
             System.out.println("Song '" + newSong.getTitle() + "' added at Pos. " + position + ".");
         }
+    }
+
+    public void deleteSong(String title) {
+        if (head == null) {
+            System.err.println("There are no songs in this playlist yet!");
+            return;
+        }
+
+        Node nodeToDelete = null;
+        Node searchNode = head;
+
+        while (searchNode != null) {
+            if (searchNode.song.getTitle().equals(title)) {
+                nodeToDelete = searchNode;
+                break;
+            }
+            searchNode = searchNode.next;
+        }
+
+        if (nodeToDelete == null) {
+            System.err.println("Song '" + title + "' not found in this playlist!");
+            return;
+        }
+
+        if (currentSongNode == nodeToDelete) {
+            if (nodeToDelete.next != null) {
+                currentSongNode = nodeToDelete.next;
+            } else if (nodeToDelete.previous != null) {
+                currentSongNode = nodeToDelete.previous;
+            } else {
+                currentSongNode = null;
+            }
+        }
+
+        if (nodeToDelete.previous == null && nodeToDelete.next == null) {
+
+            head = null;
+            tail = null;
+        } else if (nodeToDelete.previous == null) {
+            head = nodeToDelete.next;
+            head.previous = null;
+        } else if (nodeToDelete.next == null) {
+            tail = nodeToDelete.previous;
+            tail.next = null;
+        } else {
+            nodeToDelete.previous.next = nodeToDelete.next;
+            nodeToDelete.next.previous = nodeToDelete.previous;
+        }
+
+        size--;
+        System.out.println("Song '" + title + "' deleted successfully!");
+    }
+
+    public void deleteSong(int index) {
+        if (index < 0 || index >= size) {
+            System.err.println("Invalid index!");
+            return;
+        }
+
+        if (head == null) {
+            System.err.println("Playlist is empty!");
+            return;
+        }
+        int iterationCounter = 0;
+        Node currentNode = head;
+
+        while (currentNode != null) {
+            if (iterationCounter == index) {
+                if (currentNode == head && currentNode == tail) {
+                    head = null;
+                    tail = null;
+                } else if (currentNode == head) {
+                    head = currentNode.next;
+                    head.previous = null;
+                } else if (currentNode == tail) {
+                    tail = currentNode.previous;
+                    tail.next = null;
+                } else {
+                    currentNode.previous.next = currentNode.next;
+                    currentNode.next.previous = currentNode.previous;
+                }
+
+                if (currentSongNode == currentNode) {
+                    if (currentNode.next != null) {
+                        currentSongNode = currentNode.next;
+                    } else if (currentNode.previous != null) {
+                        currentSongNode = currentNode.previous;
+                    } else {
+                        currentSongNode = null;
+                    }
+                }
+
+                size--;
+                System.out.println("Song deleted successfully!");
+                break;
+            } else {
+                iterationCounter++;
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    public void deleteFirstSong() {
+        if (head == null) {
+            System.err.println("This playlist is empty!");
+            return;
+        }
+
+        Node oldHead = head;
+
+        if (head.equals(tail)) {
+            head = null;
+            tail = null;
+            currentSongNode = null;
+        } else {
+            head = head.next;
+            head.previous = null;
+
+            if (currentSongNode == oldHead) {
+                currentSongNode = head;
+            }
+        }
+        size--;
+        System.out.println("First song deleted successfully!");
+    }
+
+    public void deleteLastSong() {
+        if (tail == null) {
+            System.err.println("This playlist is empty!");
+            return;
+        }
+
+        Node oldTail = tail;
+
+        if (tail == head) {
+            head = null;
+            tail = null;
+            currentSongNode = null;
+        } else {
+            tail = tail.previous;
+            tail.next = null;
+
+            if (currentSongNode == oldTail) {
+                currentSongNode = tail;
+            }
+        }
+        size--;
+        System.out.println("Last song deleted successfully!");
+    }
+
+    public void listPlaylist() {
+        if (head == null) {
+            System.err.println("Nothing to show here!");
+            return;
+        }
+
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.song);
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void showSizeOfPlaylist() {
+        System.out.println(size);
+    }
+
+    public void goToNextSong() {
+        if (head == null) {
+            System.err.println("No songs in this playlist!");
+            return;
+        }
+
+        if (currentSongNode == null) {
+            currentSongNode = head;
+            System.out.println("Moved to the first song!");
+        } else if (head == tail) {
+            System.err.println("No more songs in this playlist!");
+        } else if (currentSongNode == tail) {
+            currentSongNode = head;
+            System.out.println("Reached the end of the playlist, moving to the first song.");
+        } else {
+            currentSongNode = currentSongNode.next;
+            System.out.println("Successfully moved to the next song!");
+        }
+    }
+
+    public void goToPreviousSong() {
+        if (tail == null) {
+            System.err.println("No songs in this playlist!");
+            return;
+        }
+
+        if (currentSongNode == null) {
+            currentSongNode = head;
+            System.out.println("Moved to the first song!");
+        } else if (tail == head) {
+            System.err.println("No more songs in this playlist!");
+        } else if (currentSongNode == head) {
+            currentSongNode = tail;
+            System.out.println("Reached the beginning of the playlist, moving to the last song.");
+        } else {
+            currentSongNode = currentSongNode.previous;
+            System.out.println("Successfully moved to the previous song!");
+        }
+    }
+
+    public void playCurrentSong() {
+        if (head == null) {
+            System.err.println("No songs to play in this playlist!");
+            return;
+        }
+
+        if (currentSongNode == null) {
+            currentSongNode = head;
+        }
+        System.out.println("Now playing: " + currentSongNode.song.getTitle());
+    }
+
+    public boolean doesSongExist(String title) {
+        if (head == null) {
+            System.err.println("There are no songs in this playlist!");
+            return false;
+        }
+
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.song.getTitle().equals(title)) {
+                return true;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isPlaylistEmpty() {
+        return head == null;
     }
 }
